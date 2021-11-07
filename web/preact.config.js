f93620d11f92e29787df8b707ef8b029e2a3f35e
Dean Davidson <ddavidson85@gmail.com>
2021-11-07 10:43:26 -0800

    Fix for production build

--- web/preact.config.js
+++ web/preact.config.js
@@ -1,8 +1,12 @@
 export default (config, env, helpers) => {
-  config.devServer.proxy = [
-    {
-      path: '/api/**',
-      target: 'http://localhost:8090/',
-    }
-  ];
+  // Only set up proxy if dev server exists
+  if (config.devServer) {
+    console.log('Setting dev server proxy settings');
+    config.devServer.proxy = [
+      {
+        path: '/api/**',
+        target: 'http://localhost:8090/',
+      }
+    ];
+  }
 }
